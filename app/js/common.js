4bd3cf5e26c5daf418e40f1bcbe0df13bc38bd03
Pavel <pasha.bpv@gmail.com>
2018-04-24 16:30:21 +0300

    add slider

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,15 @@
 $(function() {
 
 	// Custom JS
+	$('.owl-carousel').owlCarousel({
+		items:1,
+    lazyLoad:true,
+    loop:true,
+		margin:10,
+		nav:true,
+		dots: true,
+		navText: 	['<i class="fas fa-angle-left"></i>' , '<i class="fas fa-angle-right"></i>']
+})
 
-});
+
+});//End
